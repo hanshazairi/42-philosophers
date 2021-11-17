054f3c3fe668972ba8b28a1466c1310814e64527
Hans Hazairi <hanshazairi@gmail.com>
2021-11-17 16:56:47 +0800

    chore: add structs to remove global vars

--- philo/philo.c
+++ philo/philo.c
@@ -6,22 +6,18 @@
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 01:04:32 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/11/17 14:36:44 by hbaddrul         ###   ########.fr       */
+/*   Updated: 2021/11/17 16:50:02 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/time.h>
 #include "philo.h"
 
-#define PHILOS 5
-
-pthread_t		g_philos[PHILOS];
-pthread_mutex_t	g_forks[PHILOS];
-
-long	get_ts(void)
+static long	get_ts(void)
 {
 	long			ts;
 	struct timeval	s_tv;
@@ -31,68 +27,87 @@ long	get_ts(void)
 	return (ts);
 }
 
-void	*live(void *arg)
+static void	*live(void *arg)
 {
-	int				i;
-	int				x;
-	pthread_mutex_t	fork_1;
-	pthread_mutex_t	fork_2;
+	int		runs;
+	t_philo	*philo;
 
-	i = (size_t)arg;
-	x = 0;
-	if (i != PHILOS - 1)
+	philo = (t_philo *)arg;
+	runs = 0;
+	while (runs++ < 2)
 	{
-		fork_1 = g_forks[i];
-		fork_2 = g_forks[i + 1];
-	}
-	else
-	{
-		fork_1 = g_forks[0];
-		fork_2 = g_forks[i];
-	}
-	while (x++ < 2)
-	{
-		pthread_mutex_lock(&fork_1);
-		printf("%ld %d has taken a fork\n", get_ts(), i + 1);
-		pthread_mutex_lock(&fork_2);
-		printf("%ld %d has taken a fork\n", get_ts(), i + 1);
-		printf("%ld %d is eating\n", get_ts(), i + 1);
-		printf("%ld %d is sleeping\n", get_ts(), i + 1);
-		pthread_mutex_unlock(&fork_1);
-		pthread_mutex_unlock(&fork_2);
+		pthread_mutex_lock(philo->fork_1);
+		printf("%ld %d has taken a fork\n", get_ts(), philo->seat);
+		pthread_mutex_lock(philo->fork_2);
+		printf("%ld %d has taken a fork\n", get_ts(), philo->seat);
+		printf("%ld %d is eating\n", get_ts(), philo->seat);
+		printf("%ld %d is sleeping\n", get_ts(), philo->seat);
+		pthread_mutex_unlock(philo->fork_1);
+		pthread_mutex_unlock(philo->fork_2);
 		usleep(1000);
-		printf("%ld %d is thinking\n", get_ts(), i + 1);
+		printf("%ld %d is thinking\n", get_ts(), philo->seat);
 	}
 	return (0);
 }
 
-void	pthread_helper(void *f)
+static void	pthread_helper(void *f, t_table *table)
 {
 	int		i;
 	int		error;
 
 	i = 0;
-	while (i < PHILOS)
+	while (i < table->pax)
 	{
 		if (f == pthread_mutex_init)
-			error = ((t_mutex_init)f)(&g_forks[i], 0);
+			error = ((t_mutex_init)f)(&table->forks[i], 0);
 		else if (f == pthread_create)
-			error = ((t_create)f)(&g_philos[i], 0, live, (void *)(size_t)i);
+		{
+			table->philos[i].seat = i + 1;
+			table->philos[i].fork_1 = &table->forks[i];
+			table->philos[i].fork_2 = &table->forks[i + 1];
+			if (table->philos[i].seat == table->pax)
+			{
+				table->philos[i].fork_1 = &table->forks[0];
+				table->philos[i].fork_2 = &table->forks[i];
+			}
+			error = ((t_create)f)(&table->philos[i].philo, 0, live, (void *)&table->philos[i]);
+		}
 		else if (f == pthread_join)
-			error = ((t_join)f)(g_philos[i], 0);
+			error = ((t_join)f)(table->philos[i].philo, 0);
 		else if (f == pthread_mutex_destroy)
-			error = ((t_mutex_destroy)f)(&g_forks[i]);
+			error = ((t_mutex_destroy)f)(&table->forks[i]);
 		if (error != 0)
 			printf("Error\n");
 		++i;
 	}
 }
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	pthread_helper(pthread_mutex_init);
-	pthread_helper(pthread_create);
-	pthread_helper(pthread_join);
-	pthread_helper(pthread_mutex_destroy);
+	t_table	table;
+
+	if (argc == 2)
+	{
+		table.pax = ft_atoi(argv[1]);
+		table.philos = malloc(sizeof(t_philo) * table.pax);
+		if (!table.philos)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		table.forks = malloc(sizeof(pthread_mutex_t) * table.pax);
+		if (!table.forks)
+		{
+			printf("Error\n");
+			free(table.philos);
+			return (1);
+		}
+		pthread_helper(pthread_mutex_init, &table);
+		pthread_helper(pthread_create, &table);
+		pthread_helper(pthread_join, &table);
+		pthread_helper(pthread_mutex_destroy, &table);
+		free(table.forks);
+		free(table.philos);
+	}
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 14:25:52 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/11/17 14:39:32 by hbaddrul         ###   ########.fr       */
+/*   Updated: 2021/11/17 16:12:30 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,28 @@
 
 # include <pthread.h>
 
+typedef struct s_philo
+{
+	int				seat;
+	pthread_t		philo;
+	pthread_mutex_t	*fork_1;
+	pthread_mutex_t	*fork_2;
+}	t_philo;
+
+typedef struct s_table
+{
+	int				pax;
+	t_philo			*philos;
+	pthread_mutex_t	*forks;
+}	t_table;
+
 typedef int	(*t_mutex_init)(pthread_mutex_t *, const pthread_mutexattr_t *);
 typedef int	(*t_create)(pthread_t *, const pthread_attr_t *, \
 	void *(*start_routine)(void *), void *);
 typedef int	(*t_join)(pthread_t, void **value_ptr);
 typedef int	(*t_mutex_destroy)(pthread_mutex_t *);
+
+/* utils.c */
+int	ft_atoi(const char *str);
 
 #endif
